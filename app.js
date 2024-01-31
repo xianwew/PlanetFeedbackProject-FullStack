fa0f04de5e6b4ac8ec0c2c539bef746b82b85c9c
xianwew <50687225+xianwew@users.noreply.github.com>
2024-01-30 21:36:19 -0500

    seperated form validation js to another file

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
 
 app.use('/earthExplorer', earthExplorer);
 app.use('/earthExplorer/:id/reviews', reviews);
